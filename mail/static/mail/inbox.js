0765248a7f76b6344edd9f7b1bb06724ea826b02
Adrian Joniak <gimbi15@gmail.com>
2023-11-08 17:14:38 +0100

    added function to send email and made first steps in mailboxes

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -15,6 +16,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#send').addEventListener('click', send_email);
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -30,4 +32,35 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log(emails);  
+
+    emails.forEach(element => {
+      console.log(element.body); 
+    });
+});
+
+}
+
+function send_email() {
+  rec = document.querySelector('#compose-recipients').value;
+  sub = document.querySelector('#compose-subject').value;
+  bod = document.querySelector("#compose-body").value;
+  console.log(`${rec}, ${sub}, ${bod}`);
+  fetch('emails',{
+    method : "POST",
+    body: JSON.stringify({
+      recipients: rec,
+      subject: sub,
+      body: bod 
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log(result)
+  });
+  load_mailbox('sent');
 }
